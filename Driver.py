731cfb5a24e2724b1c81ccf701b65a001534c65e
NieShichao <shichaon@andrew.cmu.edu>
2019-04-27 16:58:16 -0400

    fix database connection

--- Driver.py
+++ Driver.py
@@ -2,24 +2,22 @@
 
 import json
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-import mysql.connector
-
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="username",
-  passwd="password"
-)
-
-mycursor = mydb.cursor()
+from sqlalchemy import create_engine, and_, or_
+from sqlalchemy.orm import sessionmaker
+from database import Base, Customer, Vendor, Image, Menu, Orders
 
 app = Flask(__name__)
+
+engine = create_engine('mysql://test:password@35.245.224.212:3306/mobile_canteen')
+Base.metadata.bind = engine
 
 @app.route('/register/', methods = ['GET', "POST"])
 def user_register():
     if request.method == 'POST':
         data = request.get_data()
+        print(data)
         # TODO: register
-        return jsonify()
+        return jsonify({"token": "EHAS12389ASDHJK"})
     else:
         return 'POST only'
 
@@ -35,16 +33,14 @@ def user_login():
 @app.route('/menus/')
 def get_menus():
     if request.method == 'GET':
-        mycursor.execute("SELECT * FROM menu")
-        myresult = mycursor.fetchall()
+        
         # TODO: return today's menu
         return jsonify()
 
 @app.route('/menus/<int:dish_id>/')
 def get_dish():
     if request.method == 'GET':
-        mycursor.execute("SELECT * FROM menu WHERE dish_id = %d" % dish_id)
-        myresult = mycursor.fetchall()
+        
         # TODO: return certain dish
         return jsonify()
 
@@ -57,40 +53,35 @@ def publish_dish():
 @app.route('/vendors/')
 def get_vendors():
     if request.method == 'GET':
-        mycursor.execute("SELECT * FROM vendor")
-        myresult = mycursor.fetchall()
+        
         # TODO: return vendors
         return jsonify()
 
 @app.route('/vendors/<int:vendor_id>/')
 def get_vendor():
     if request.method == 'GET':
-        mycursor.execute("SELECT * FROM vendor WHERE vendor_id = %d" % vendor_id)
-        myresult = mycursor.fetchall()
+        
         # TODO: return vendor
         return jsonify()
 
 @app.route('/vendors/<int:vendor_id>/menu/')
 def get_vendor_offerings():
     if request.method == 'GET':
-        mycursor.execute("SELECT * FROM menu WHERE vendor_id = %d" % vendor_id)
-        myresult = mycursor.fetchall()
+        
         # TODO: return vendor's offerings
         return jsonify()
 
 @app.route('/orders/<int:customer_id>/')
 def get_orders():
     if request.method == 'GET':
-        mycursor.execute("SELECT * FROM order WHERE customer_id = %d" % customer_id)
-        myresult = mycursor.fetchall()
+        
         # TODO: return orders
         return jsonify()
 
 @app.route('/orders/<int:customer_id>/<int:dish_id>/')
 def get_order():
     if request.method == 'GET':
-        mycursor.execute("SELECT * FROM order WHERE customer_id = %d AND dish_id = %d" % (customer_id, dish_id))
-        myresult = mycursor.fetchall()
+        
         # TODO: return certain orders
         return jsonify()
 
@@ -103,7 +94,6 @@ def place_order():
 @app.route('/status/<int:vendor_id>/')
 def get_order():
     if request.method == 'GET':
-        mycursor.execute("SELECT * FROM status WHERE vendor_id = %d" % vendor_id)
-        myresult = mycursor.fetchall()
+        
         # TODO: return certain orders
         return jsonify()
